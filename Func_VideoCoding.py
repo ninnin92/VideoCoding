4826de364e3bbecb5423690934d69f0854274050
ninnin92 <ninnin92.28@gmail.com>
2017-04-17 16:31:20 +0900

    UIを表示できるように

--- Func_VideoCoding.py
+++ Func_VideoCoding.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import pygame
-from pygame.locals import *
-
-switch = -1
-
-
-def coding_switch(self):
-    switch = self.switch*-1
-    return switch
+import sys
+import ui_videocoding  # designerで作ったUIを変換してソースとして読み込み
+from PyQt5.QtGui import QKeyEvent
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QMessageBox
 
 
-def keypress_coding(self):
-    pass
+class MainWindow(QMainWindow, ui_videocoding.Ui_MainWindow):
+    # 初期設定
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        # setupUi というメソッドが定義されているので実行する
+        # これで設置したウィジェットなどがインスタンス化される
+        self.setupUi(self)
+        # 継承したので self.名前 でアクセスできる
+        self.setWindowTitle("VideoCoding v0.1") # ウィンドウタイトル
 
+    def keyPressEvent(self, Event):
+        print(Event.key())
+        if Event.key() == Qt.Key_Space:
+            print('Space pressed')
+        else:
+            super().keyPressEvent(Event)
 
-def save_file(self):
-    pass
+    def keyReleaseEvent(self, Event):
+        print(Event.key())
+        if Event.key() == Qt.Key_Space:
+            print('Space Released')
+        else:
+            super().keyReleaseEvent(Event)
 
 if __name__ == '__main__':
-    pygame.init()
-    waitingKeyPress = True
-    while waitingKeyPress:
-        for e in pygame.event.get():
-            if e.type == pygame.KEYDOWN:
-                print("hoge")
-                waitingKeyPress = False
-            else:
-                print("no press")
+    app = QApplication(sys.argv)  # キー入力やイベント発生などのリソース管理
+    main_window = MainWindow()    # ウィンドウインスタンスの作成
+    main_window.show()            # ウィンドウの描画
+    sys.exit(app.exec_())         # アプリのイベントループが終わったらプログラムを修了
