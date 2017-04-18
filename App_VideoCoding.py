32ce174672b25c972d7259b4fe83466f8f17b743
ninnin92 <ninnin92.28@gmail.com>
2017-04-19 02:05:02 +0900

    キー押しの検出、スイッチ機能仮組み

--- App_VideoCoding.py
+++ App_VideoCoding.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import ui_ageindays # designerで作ったUIを変換してソースとして読み込み
+import ui_videocoding  # designerで作ったUIを変換してソースとして読み込み
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QMessageBox
 
 
-class MainWindow(QMainWindow, ui_ageindays.Ui_AgeInDays):
-
+class MainWindow(QMainWindow, ui_videocoding.Ui_MainWindow):
     # 初期設定
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -15,11 +15,26 @@ class MainWindow(QMainWindow, ui_ageindays.Ui_AgeInDays):
         # これで設置したウィジェットなどがインスタンス化される
         self.setupUi(self)
         # 継承したので self.名前 でアクセスできる
-        self.setWindowTitle("VideoCoding v1.0") # ウィンドウタイトル
+        self.setWindowTitle("VideoCoding v0.1")  # ウィンドウタイトル
 
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Control:
+            print('Ctrl pressed!')
+        else:
+            pass
+
+    def keyReleaseEvent(self, event):
+        # 長押しのリピートを解除
+        if event.isAutoRepeat():
+            return
+
+        if event.key() == Qt.Key_Control:
+            print('Ctrl Released')
+        else:
+            pass
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv) # キー入力やイベント発生などのリソース管理
-    main_window = MainWindow()   # ウィンドウインスタンスの作成
-    main_window.show()           # ウィンドウの描画
-    sys.exit(app.exec_())        # アプリのイベントループが終わったらプログラムを修了
+    app = QApplication(sys.argv)  # キー入力やイベント発生などのリソース管理
+    main_window = MainWindow()    # ウィンドウインスタンスの作成
+    main_window.show()            # ウィンドウの描画
+    sys.exit(app.exec_())         # アプリのイベントループが終わったらプログラムを修了
--- Func_VideoCoding.py
+++ Func_VideoCoding.py
@@ -2,35 +2,59 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import time
 import ui_videocoding  # designerで作ったUIを変換してソースとして読み込み
-from PyQt5.QtGui import QKeyEvent
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QMessageBox
 
 
-class MainWindow(QMainWindow, ui_videocoding.Ui_MainWindow):
+class MainWindow(QMainWindow, ui_videocoding.Ui_VideoCoding):
     # 初期設定
+    switch = -1
+
     def __init__(self):
         super(MainWindow, self).__init__()
         # setupUi というメソッドが定義されているので実行する
         # これで設置したウィジェットなどがインスタンス化される
         self.setupUi(self)
         # 継承したので self.名前 でアクセスできる
-        self.setWindowTitle("VideoCoding v0.1") # ウィンドウタイトル
+        self.setWindowTitle("VideoCoding v0.1")  # ウィンドウタイトル
+        # 操作と関数のひも付け
+        self.pushButton_start.clicked.connect(self.start_switch)
+        self.pushButton_stop.clicked.connect(self.stop_switch)
 
-    def keyPressEvent(self, Event):
-        print(Event.key())
-        if Event.key() == Qt.Key_Space:
-            print('Space pressed')
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Control:
+            global press_time         # 関数外で使うのでグローバルに
+            press_time = time.time()  # keyを押した時点での時刻
+            print('Ctrl pressed!')
         else:
-            super().keyPressEvent(Event)
+            pass
 
-    def keyReleaseEvent(self, Event):
-        print(Event.key())
-        if Event.key() == Qt.Key_Space:
-            print('Space Released')
+    def keyReleaseEvent(self, event):
+        # 長押しのリピートを解除
+        if event.isAutoRepeat():
+            return
+
+        if event.key() == Qt.Key_Control:
+            global release_time         # 関数外で使うのでグローバルに
+            release_time = time.time()  # keyを離した時点での時刻
+            print('Ctrl Released' + str(self.time_count()))
         else:
-            super().keyReleaseEvent(Event)
+            pass
+
+    def time_count(self):
+        duration = release_time - press_time
+        return duration
+
+    def start_switch(self):
+        self.switch = self.switch * -1
+        print(self.switch)
+
+    def stop_switch(self):
+        self.switch = self.switch * -1
+        print(self.switch)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)  # キー入力やイベント発生などのリソース管理
