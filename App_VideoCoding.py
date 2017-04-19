eeac249773bf0fd444abb9cdd1f0634920b86927
ninnin92 <ninnin92.28@gmail.com>
2017-04-19 16:07:42 +0900

    VidoeCoding beta版：最低限必要な機能はすべて実装

--- App_VideoCoding.py
+++ App_VideoCoding.py
@@ -2,12 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import time
+import datetime as dt
 import ui_videocoding  # designerで作ったUIを変換してソースとして読み込み
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
-class MainWindow(QMainWindow, ui_videocoding.Ui_MainWindow):
+class MainWindow(QMainWindow, ui_videocoding.Ui_VideoCoding):
+    # 関数設定
+    switch = False         # on = True, off = Falseで設定　初期設定はOFF
+    durations_list = []    # 複数回ボタンが押された時のdurationの仮置き場
+    press_time = 0         # 時間の計測用
+    release_time = 0       # 時間の計測用
+    ID = None              # 動画情報入力
+    Trial = None           # 動画情報入力
+    fp = None              # 出力用csvファイル
+
     # 初期設定
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -15,23 +26,113 @@ class MainWindow(QMainWindow, ui_videocoding.Ui_MainWindow):
         # これで設置したウィジェットなどがインスタンス化される
         self.setupUi(self)
         # 継承したので self.名前 でアクセスできる
-        self.setWindowTitle("VideoCoding v0.1")  # ウィンドウタイトル
+        self.setWindowTitle("VideoCoding v1.0")  # ウィンドウタイトル
+        # ボタンを押す操作と関数のひも付け
+        self.pushButton_Start.clicked.connect(self.start_switch)
+        self.pushButton_Stop.clicked.connect(self.stop_switch)
+        # textBrowserにindexを追加
+        self.textBrowser.append("[  ID  ] [  Trial  ] [  Duration  ]")
 
+        # 出力用のcsvファイルを作成
+        app_time = "{0:%y-%m-%d_%H-%M-%S}".format(dt.datetime.now())  # formatを使うと一行で済んで便利
+        self.fp = open("VideoCoding_" + app_time + ".csv", "w")       # csvファイルを起動時刻で作成
+
+        # csvファイルにindexを書き込む
+        index = ["Name_ID", "Trial", "Duration"]
+        for i in index:
+            self.fp.write("%s," % i)  # リストから1つずつ書き込み
+        self.fp.write("\n")
+        self.fp.flush()
+
+    # Qwidgetの組み込みの関数：ボタンを押した時
     def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Control:
-            print('Ctrl pressed!')
+        # コーディングを開始しているときだけ反応
+        if self.switch:
+            # 長押しのリピートを解除
+            if event.isAutoRepeat():
+                return
+            # ctrlキーを押した瞬間の時間を計測
+            if event.key() == Qt.Key_Control:
+                self.press_time = time.time()  # keyを押した時点での時刻
+                print('Ctrl Pressed')
+            else:
+                pass
         else:
             pass
 
+    # Qwidgetの組み込みの関数：ボタンを離した時
     def keyReleaseEvent(self, event):
-        # 長押しのリピートを解除
-        if event.isAutoRepeat():
-            return
-
-        if event.key() == Qt.Key_Control:
-            print('Ctrl Released')
+        # コーディングを開始しているときだけ反応
+        if self.switch:
+            # 長押しのリピートを解除
+            if event.isAutoRepeat():
+                return
+            # ctrlキーを押した瞬間の時間を計測
+            if event.key() == Qt.Key_Control:
+                self.release_time = time.time()  # keyを離した時点での時刻
+                print('Ctrl Released' + str(self.time_count()))
+            else:
+                pass
         else:
             pass
+
+    # ボタンを離した時刻からボタンを押した時の時刻を引き算、リストに追加
+    def time_count(self):
+        duration = self.release_time - self.press_time
+        self.durations_list.append(duration)
+        return duration
+
+    # コーディングを開始する
+    def start_switch(self):
+        # on/off の切り替え
+        self.switch = True
+        # onのときはボタンの色を変える
+        self.pushButton_Start.setStyleSheet('background-color: #FFD25A')
+        # onのときは入力できないようにしておく
+        self.lineEdit_ID.setEnabled(False)
+        self.lineEdit_Trial.setEnabled(False)
+
+        # Name,Trialに入力があるかチェック（なければNoneを追加）
+        if len(self.lineEdit_ID.text()) > 0:
+            self.ID = self.lineEdit_ID.text()
+        else:
+            self.ID = "None"
+
+        if len(self.lineEdit_Trial.text()) > 0:
+            self.Trial = self.lineEdit_Trial.text()
+        else:
+            self.Trial = "None"
+
+        print(self.switch)
+
+    # コーディングを終了する
+    def stop_switch(self):
+        # on/off の切り替え
+        self.switch = False
+        # offになったらボタンの色を戻す
+        self.pushButton_Start.setStyleSheet('background-color: None')
+        # offのときは入力できるようにしておく
+        self.lineEdit_ID.setEnabled(True)
+        self.lineEdit_Trial.setEnabled(True)
+
+        # 合計の時間を計算
+        total_duration = sum(self.durations_list)
+        # TextBrowserに動画情報と計測時間を追加する
+        self.textBrowser.append(self.ID + ",  " + self.Trial + ",  " + str(total_duration))
+
+        # csvファイルに解析結果を書き込み
+        for x in [str(self.ID), str(self.Trial), str(total_duration)]:
+            self.fp.write("%s," % x)  # リストから1つずつ書き込み
+        self.fp.write("\n")
+        self.fp.flush()
+
+        print(self.switch)
+        print(self.durations_list)
+        print(total_duration)
+
+        # 初期化
+        self.durations_list = []
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)  # キー入力やイベント発生などのリソース管理
